087551fa400deed92e34137a6d08782fdea032e5
shrinivassab <shrinivassab@gmail.com>
2021-03-11 14:29:03 -0500

    -- Session and Cookies

--- Shop_Project/app.js
+++ Shop_Project/app.js
@@ -3,24 +3,39 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
-// const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://shrinivassab-node:h6DadFR1LMvt7Dqi@cluster0.uvxvg.mongodb.net/shop?retryWrites=true&w=majority'
+
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const authRoutes = require('./routes/auth');
+// Registering
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}))
 
 app.use((req, res, next) => {
-    User.findById('60370891e9edc955886f9830')
+    User.findById('5bab316ce0a7c75f783cb8a8')
         .then(user => {
             req.user = user;
             next();
@@ -30,10 +45,14 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://shrinivassab-node:h6DadFR1LMvt7Dqi@cluster0.uvxvg.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose
+    .connect(
+        MONGODB_URI
+    )
     .then(result => {
         User.findOne().then(user => {
             if(!user) {
